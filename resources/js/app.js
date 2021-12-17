d8e1e5632e6748031daa5ae156b7a6aefac4a0c5
Tanvir <r3dr0k37@gmail.com>
2021-12-17 22:47:38 +0600

    hello

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import Vue from "vue";
+import HelloWorld from "./component/HelloWorld";
+
+Vue.component("hello-world", HelloWorld);
+
+const app = new Vue({
+    el: "#app",
+}).$mount("#app");
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,8 +20,12 @@
             }
         </style>
     </head>
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <body class="antialiased" >
+        <div id="app">
+
+            <hello-world/>
+        </div>
+        {{-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -127,6 +131,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </body>
+    <script src="/app.js"></script>
 </html>
